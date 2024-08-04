f3a177fcb4cf287d5920228eba2bf352cfe73159
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-08-04 20:44:38 +0530

    Add files via upload

---
+++ Lecture 12 CipherSchool.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int password;
+	
+	
+	
+	
+	int i;
+	for(i=0;  ;i++)
+	{
+		cout<<i<<" ";
+		if(i>100) break; // This statement gets executed when i is 101
+	}
+	
+	
+	
+//	while(1) // the condition in the while loop, untill it is true, the loop will run
+//	{
+//		cout<<"Choclates"<<endl;
+//		count++;
+//		if(count>100) break;
+//	}
+//	
+	
+//	do{
+//		cin>>password;
+//	}while(password<999999)
+	
+	
+//	
+//	cout<<"enter the password: "<<endl;
+//	cin>>password;
+//	
+//	while(password<999999)
+//	{
+//		cout<<"The password does not meet the required condition, please enter the password again "<<endl;
+//		cin>>password;
+//	}
+//	cout<<"The user has now entered a correct password"<<endl;
+	
+	
+	return 0;
+}
